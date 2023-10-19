9aaf93be2808cef352f6fbb430ae5dff317ccfaa
Amirametwally <amira.metwally56@gmail.com>
2023-10-19 17:39:59 +0300

    adding function addnode and addqueue

--- tools.c
+++ tools.c
@@ -18,3 +18,79 @@ void stack_free(stack_t *head)
 		head = temp;
 	}
 }
+
+/**
+ * _queue - prints the top
+ * @head: stack head
+ * @count: line_number
+ *
+ * Return: nothing
+ */
+void _queue(stack_t **head, unsigned int count)
+{
+	(void)head;
+	(void)count;
+	info.f_change = 1;
+}
+
+/**
+ * addqueue - add node at the tail stack
+ * @head: head of the stack
+ * @num: newValue
+ *
+ * Return: nothing
+ */
+void addqueue(stack_t **head, int num)
+{
+	stack_t *new, *ptr;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+	}
+	new->n = num;
+	new->next = NULL;
+	if (ptr)
+	{
+		while (ptr->next)
+			ptr = ptr->next;
+	}
+	if (ptr == NULL)
+	{
+		*head = new;
+		new->prev = NULL;
+	}
+	else
+	{
+		ptr->next = new;
+		new->prev = ptr;
+	}
+}
+
+/**
+ * addnode - add node to the head stack
+ * @head: head of atack
+ * @num: newValue
+ *
+ * Return: nothing
+ */
+void addnode(stack_t **head, num)
+{
+	stack_t *new, *ptr;
+
+	ptr = *head;
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+
+	if (ptr)
+		ptr->prev = new;
+	new->n = num;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+}
