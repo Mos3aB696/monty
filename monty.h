ead397238728e7e78beb4eda3cf2e9587858487d
Mos3aB696 <mos3ab696@gmail.com>
2023-10-19 22:09:06 +0300

    add prototype

--- monty.h
+++ monty.h
@@ -77,6 +77,6 @@ void _mod(stack_t **head, unsigned int line_number);
 void _mul(stack_t **head, unsigned int line_number);
 void _pchar(stack_t **head, unsigned int line_number);
 void _pstr(stack_t **head, unsigned int line_number);
-
+void _rotl(stack_t **head, unsigned int line_number);
 
 #endif
