39d531c1a852a7b6333a60b06531dabde07d0354
Mos3aB696 <mos3ab696@gmail.com>
2023-10-19 22:54:48 +0300

    last update

--- execute.c
+++ execute.c
@@ -2,48 +2,47 @@
 
 /**
  * _execute - executes the opcode
- * @stack: stack
- * @line_number: line_number
- * @file: pointer to monty file
- * @line_content: line content
- *
- * Return: nothing
+ * @content: line content
+ * @head: head of stack
+ * @line_number: line number
+ * @file: monty file
+ * 
+ * Return: 1
  */
-int _execute(char *line_content, stack_t **stack, unsigned int line_number,
-						 FILE *file)
+
+int _execute(char *content, stack_t **head, unsigned int line_number, FILE *file)
 {
-	instruction_t zalta[] = {
-			{"push", _push}, {"pall", _pall},
-			{"pop", _pop}, {"swap", _swap},
-			{"add", _add}, {"nop", _nop},
-			{"queue", _queue}, {"pint", _pint}, {NULL, NULL},
-			{"sub", _sub}, {"div", _div}, {"mul", _mul},
-			{"mod", _mod}, {"pchar", _pchar}, {"pstr", _pstr},
-			{"rotl", _rotl}, {"rotr", _rotr}, {"stack", _stack},
-			{"queue", _queue}
+	instruction_t fun[] = {
+			{"push", _push}, {"pall", _pall}, {"pint", _pint},
+			{"pop", _pop}, {"swap", _swap}, {"add", _add},
+			{"nop", _nop}, {"sub", _sub}, {"div", _div}, {"mul", _mul}, 
+			{"mod", _mod}, {"pchar", _pchar}, {"pstr", _pstr}, 
+			{"rotl", _rotl}, {"rotr", _rotr}, {"queue", _queue}, 
+			{"stack", _stack}, {NULL, NULL}
 			};
 	unsigned int i = 0;
-	char *op;
+	char *op_code;
 
-	op = strtok(line_content, " \n\t");
-	if (op && op[0] == '#')
+	op_code = strtok(content, " \n\t");
+	if (op_code && op_code[0] == '#')
 		return (0);
 	info.arg = strtok(NULL, " \n\t");
-	while (zalta[i].opcode && op)
+	while (fun[i].opcode && op_code)
 	{
-		if (strcmp(op, zalta[i].opcode) == 0)
+		if (strcmp(op_code, fun[i].opcode) == 0)
 		{
-			zalta[i].f(stack, line_number);
+			fun[i].f(head, line_number);
+			;
 			return (0);
 		}
 		i++;
 	}
-	if (op && zalta[i].opcode == NULL)
+	if (op_code && fun[i].opcode == NULL)
 	{
-		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, op);
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, op_code);
 		fclose(file);
-		free(line_content);
-		stack_free(*stack);
+		free(content);
+		stack_free(*head);
 		exit(EXIT_FAILURE);
 	}
 	return (1);
